d9381ff71ff6b087fbfa908a79d5cc056596be65
Nodirbek Ergashev <Nodirbek_Ergashev@epam.com>
2022-01-14 20:49:20 +0500

    add logger package and config file

---
+++ src/loader/logger.js
@@ -0,0 +1,38 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    format: winston.format.combine(
+        winston.format.label({
+            label: 'figma-service'
+        }),
+        winston.format.timestamp(),
+        winston.format.splat(),
+        winston.format.metadata({
+            fillExcept: ['message', 'level', 'timestamp', 'label']
+        }),
+        winston.format.prettyPrint()
+    ),
+    transports: [
+        new winston.transports.File({ 
+            filename: './logs/debug.log', 
+            level: 'debug'
+        }),
+        new winston.transports.File({
+            filename: './logs/error.log',
+            level: 'error'
+        })
+    ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    const consoleLogFormat = winston.format.printf(info => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`)
+    logger.add(new winston.transports.Console({
+        format: winston.format.combine(
+            winston.format.colorize(),
+            consoleLogFormat
+        ),
+        level: 'debug'
+    }));
+}
+
+exports.logger = logger;
